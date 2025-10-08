7e28861fedbb8d16258c87038a96622197a8ecb7
Somenath Gorai <somenathgorai406@gmail.com>
2025-10-08 06:39:14 +0530

    playwright reporting

---
+++ tests/playwrightReporting.spec.js
@@ -0,0 +1,25 @@
+const { test, expect } = require('@playwright/test');
+
+test.only('Playwright Reporting', async ({ page }) => {
+    await page.goto('https://www.google.com');
+
+    //Assertion
+    await expect(page).toHaveTitle('Google');
+    //Assertion
+    await expect(page.getByRole('combobox', { name: 'Search' })).toBeVisible();
+    await page.getByRole('combobox', { name: 'Search' }).click();
+    await page.getByRole('combobox', { name: 'Search' }).fill('Hare Kri');
+    //print in console
+    console.log(await page.getByRole('combobox', { name: 'Search' }).inputValue())
+    //Ss of the search engine
+    await page.getByRole('combobox', { name: 'Search' }).screenshot({ path: 'test-results/partialcreenshot.png' })
+    //screenshot of the full page
+    await page.screenshot({ path: 'test-results/Googlescreenshot.png' });
+
+    //expect(await page.screenshot()).toMatchSnapshot('test-results/Googlescreenshot.png', { maxDiffPixels: 17262 })
+    //await page.pause();
+    //await page.getByText('hare krishna', { exact: true }).click();
+    //screenshot
+    //await page.screenshot({ 'path: google.png'})
+
+})
